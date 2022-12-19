c286f2b60dbeda841efbc670a9207cfeb2ba6ec9
Georgy <kiselev.ga@edu.spbstu.ru>
2022-12-19 12:02:46 +0300

    module8-task2

--- js/big-pictures.js
+++ js/big-pictures.js
@@ -1,55 +1,63 @@
 const bigPicture = document.querySelector('.big-picture');
-const bigPictureCancel = document.querySelector('.big-picture__cancel');
-const commentListElement = document.querySelector('.social__comments');
-const commentsFragment = document.createDocumentFragment();
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+const STEP = 5;
 
-const renderComments = (comments) => {
-  comments.forEach((comment) => {
-    const socialComment = document.querySelector('.social__comment').cloneNode(true);
-    socialComment.querySelector('.social__picture').src = comment.avatar;
-    socialComment.querySelector('.social__picture').alt = comment.name;
-    socialComment.querySelector('.social__text').textContent = comment.message;
-    commentsFragment.appendChild(socialComment);
+let postCommentsCounter = 0;
+let loadedCommentsCounter = 0;
+let commentsInPost = 0;
+
+const generateComments = (comments) => {
+  comments.array.forEach(comment => {
+    loadedCommentsCounter++;
+    const li = document.createElement('li');
+    li.classList.add('social__comment');
+
+    const img = document.createElement('img');
+    img.classList.add('social__picture');
+    img.src = comment.avatar;
+    img.alt = comment.name;
+    img.width = 35;
+    img.height = 35;
+
+    const p = document.createElement('p');
+    p.classList.add('social__text');
+    p.textContent = comment.message;
+
+    li.appendChild(img);
+    li.appendChild(p);
+    bigPicture.querySelector('.social__comments').appendChild(li);
+    if (loadedCommentsCounter === postCommentsCounter) {
+      commentsLoader.classList.add('hidden');
+    }
   });
-
-  commentListElement.innerHTML = '';
-  commentListElement.appendChild(commentsFragment);
 };
 
-const renderPicture = (picture) => {
-  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicture.querySelector('.comments-loader').classList.add('hidden');
-  bigPicture.querySelector('.big-picture__img img').src = picture.url;
-  bigPicture.querySelector('.likes-count').textContent = picture.likes;
-  bigPicture.querySelector('.social__caption').textContent = picture.description;
-  bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
-  renderComments(picture.comments);
+const cutComments = (comments) => {
+  generateComments(comments.slice(loadedCommentsCounter, loadedCommentsCounter + STEP));
+  document.querySelector('.current-comments-count').textContent = loadedCommentsCounter;
 };
 
-const closePicture = () => {
-  bigPicture.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-};
+function loadFiveComments(evt) {
+  evt.preventDefault();
+  cutComments(commentsInPost);
+}
 
-const escKeydown = (evt) => {
-  if(evt.key === 'Escape') {
-    closePicture();
-    document.removeEventListener('keydown', escKeydown);
-  }
-};
-
-const closeButton = () => {
-  closePicture();
-  bigPictureCancel.removeEventListener('click', closeButton);
-  document.removeEventListener('keydown', escKeydown);
-};
-
-const openBigPicture = (item) => {
-  document.body.classList.add('modal-open');
+const addBigPicture = (post) => {
   bigPicture.classList.remove('hidden');
-  renderPicture(item);
-  bigPictureCancel.addEventListener('click', closeButton);
-  document.addEventListener('keydown', escKeydown);
+  bigPicture.querySelector('.big-picture__img').querySelector('img').src = post.url;
+  bigPicture.querySelector('.likes-count').textContent = post.likes;
+  commentsInPost = post.comments;
+  cutComments(commentsInPost);
+  postCommentsCounter = commentsInPost.length;
+  commentsLoader.addEventListener('click', loadFiveComments);
+  bigPicture.querySelector('.comments-count').textContent = commentsInPost.length.toString();
+  bigPicture.querySelector('.social__caption').textContent = post.description;
+  document.querySelector('body').classList.add('modal-open');
 };
 
-export { openBigPicture };
+function resetComments() {
+  loadedCommentsCounter = 0;
+  postCommentsCounter = 0;
+}
+
+export {addBigPicture, bigPicture, resetComments};
